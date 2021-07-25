d6060c79dce8a9c7a66cd125ee24ce591958e46c
wenkeyang <ywksunshine@hotmail.com>
2021-07-25 23:15:33 +1000

    adjust format

--- src/main/scala/util/spark_util.scala
+++ src/main/scala/util/spark_util.scala
@@ -4,13 +4,12 @@ import org.apache.spark.sql.SparkSession
 
 object spark_util {
 
-  def get_spark_session = {
+  def get_spark_session: SparkSession = {
     val spark = SparkSession
       .builder()
       .appName(getClass.getName)
       .master("local[*]")
       .getOrCreate()
-
     spark.sparkContext.setLogLevel("ERROR")
     spark
   }
